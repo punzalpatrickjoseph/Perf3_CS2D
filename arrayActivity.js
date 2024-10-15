3cf971f4599e6c81f9cb9b5e3526ba1419e436e2
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-10-16 07:50:11 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+let favSingers = ["Adele", "Ed Sheeran", "Taylor Swift"];
+console.log(favSingers[0]); //declare the favorite singers inside the arrays
+let favNumbers = [17, 26, 13, 25];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);

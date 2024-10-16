e218290fa5651b49c449eba86035becbd661ff4e
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-10-16 08:15:25 +0800

    objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+const car = {
+    type: "Toyota",
+    model: "Supra",
+    color: "Orange"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
